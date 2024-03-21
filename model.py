1697a20613ae4e467bae78f11daf631ae012a3c8
Ibler <iblerchristoffer@gmail.com>
2024-03-21 13:42:32 +0100

    Update model.py

--- model.py
+++ model.py
@@ -5,11 +5,12 @@ import pandas as pd
 raw_data = dataprocessing.get_data()
 cleaned_data = raw_data.copy()
 reliable_data = raw_data.copy()
-print(len(raw_data.content))
-
 cleaned_data['content'] = raw_data['content'].apply(dataprocessing.text_preprocessing)
-#print(cleaned_data)
+# print(reliable_data)
+
+reliable_data = reliable_data.drop(reliable_data[~(reliable_data['type'] != 'reliable')])
 print(reliable_data)
+
 # i=0
 # while i <= (len(reliable_data.content)):
 #     if reliable_data['type'][i] != 'reliable':
@@ -18,8 +19,6 @@ print(reliable_data)
 #         i = i + 1
 # reliable_data = reliable_data.drop("id",axis=1)
 # reliable_data = reliable_data.drop(reliable_data['type'][reliable_data['type'] != 'reliable'], inplace = True)
-reliable_data = reliable_data.drop(reliable_data[~(reliable_data['type'] != 'reliable')])
-print(reliable_data)
 
 
 # reliable_data['content'] = raw_data['content'].apply(only_relaible)
