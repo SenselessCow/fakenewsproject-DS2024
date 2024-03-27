18a9b720aa76145297ce56a887c4507b16e0a0b9
Ibler <iblerchristoffer@gmail.com>
2024-03-27 20:37:08 +0100

    Update model.py
    Made it so we get same data distribution every time and fixed some stuff.. Code still does'nt work

--- model.py
+++ model.py
@@ -23,16 +23,21 @@ def map_to_group(type_value):
 # derfor er jeg stuck. jeg har også prøvet at lave alt dette med pandas .apply, men jeg var for dum..  
 def domain_to_boolean(X,reliables):
     for j in range (len(X['domain'])):
-        print(X['domain'])
         i=0
-        print(X['domain'][j])
-        print(reliables[i])
-        while X['domain'][j] != (reliables[i]):
-            i=i+1
-            if i+1 == len(reliables):
-                X['domain'][j] = 0
-        X['domain'][j] = 1
-        j=j+1
+        # print(X.loc[j, "domain"])
+        if reliables.dtype.type is np.str_:
+            if X.loc[j, "domain"] != (reliables):
+                X.loc[j, "trusted"] = 0
+            else:
+                X.loc[j, "trusted"] = 1
+        else:
+            while X.loc[j, "domain"] != (reliables[i]):
+                i=i+1
+                if i+1 == len(reliables):
+                    X.loc[j, "trusted"] = 0
+            X.loc[j, "trusted"] = 1
+            j=j+1
+        
     return X
 
 # def domain_to_boolean(domain,reliables):
@@ -49,7 +54,8 @@ def trusted_sites(a):
     for i in range (len(a['content'])):
         if a['GroupedType'][i] == 'GroupReliable':
             b.append(a['domain'][i])
-    result = np.unique(b, return_counts=False)          
+    result = np.unique(b, return_counts=False)
+    print("reliable sites found = " + str(result))   
     return result[0]
 
 raw_data = dataprocessing.get_data()
@@ -77,17 +83,62 @@ comparison_df = pd.DataFrame({'AllGroups': all_groups_percentage, 'FakeVsReliabl
 
 #IBLERS UPDATES - 
 X = clean_data
-y = clean_data['GroupedType']
+# y = clean_data['GroupedType']
 
-X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size=0.2, random_state=0)
-X_train = X_train['domain']
+#Split dataen op, den klassiske måde virkede ikke for det resulterede i at man kun kunne iterere columns men ikke rows i vores pandas dataframe..
+#Med denn måde at fordele det på bliver fordelingen lidt forskellig hver gang, men det virker. Vi skal fikse det tho
+# X_train, X_test, y_train, y_test = ms.train_test_split(X, y, test_size=0.2, random_state=0)
+
+# msk = np.random.rand(len(X)) < 0.8
+# X_trainv = X[msk]
+# X_testv = X[~msk]
+# msk = np.random.rand(len(X_trainv)) < 0.5
+# X_train = X_trainv[msk].reset_index()
+# y_train = X_trainv[~msk].reset_index()
+# msk = np.random.rand(len(X_testv)) < 0.5
+# X_test = X_testv[msk].reset_index()
+# y_test = X_testv[~msk].reset_index()
+# del X_trainv
+# del X_testv
+
+indexlist = []
+# print(indexlist[0])
+for a in range (len(X)):
+    indexlist.append(a) 
+vX_train, vX_test, vy_train, vy_test = ms.train_test_split(indexlist, indexlist, test_size=0.2, random_state=0)
+# print(vX_train)
+# print(len(X))
+# print(len(vX_train))
+# print(len(vy_train))
+# print(len(vX_test))
+# print(len(vy_test))
+# print(X.iloc[3])
+X_train = X.iloc[vX_train].reset_index()
+y_train = X.iloc[vy_train].reset_index()
+X_test = X.iloc[vX_test].reset_index()
+y_test = X.iloc[vy_test].reset_index()
+# print(X_train)
+
+# X_train = X_train['domain']
+# y_train = y_train['GroupedType']
+y_test =y_test['GroupedType']
 
 # X_test = X_test['domain'].apply(domain_to_boolean,args=(reliable_sites))
-X_test = domain_to_boolean(X_test, reliable_sites)
-print(X_test)
+X_train = domain_to_boolean(X_train, reliable_sites)
+y_train = domain_to_boolean(y_train, reliable_sites)
+# print(X_test)
+
+feature_cols = ['trusted']
+# you want all rows, and the feature_cols' columns
+X_train = X_train.loc[:, feature_cols]
+print(X_train)
+y_train = y_train.loc[:, feature_cols]
+print(y_train)
 
 #Mange data skal behandles før dette vil virke:
 model = LinearRegression()
+# print(X_train)
+# print(y_train)
 reg = model.fit(X_train, y_train)     #MEGET LANGSOM LINJE
 y_pred = reg.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
