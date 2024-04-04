33fcc43695d131eb06d37de07ad92ba12e3c8164
Yaser Hashimi <yh@cerebriu.com>
2024-04-04 13:25:47 +0200

    Merging columns into cleaned dataset

--- merge.py
+++ merge.py
@@ -4,26 +4,23 @@ import pandas as pd
 df2 = pd.read_csv('995,000_cleaned_dataset.csv', low_memory=False)
 
 # Define chunk size
-chunksize = 10000
+chunksize = 5000
 
 # Create an empty list to hold the dataframes
 dfs = []
-print('Column: {} and {}'.format('id', df2['id'].apply(type).unique()))
 total_chunks = 995000 // chunksize
 print('total number of chunks to run: {}'.format(total_chunks))
 i = 0
-
+df2['id'] = df2['id'].astype('string')
 # Load the first dataset in chunks
 for chunk in pd.read_csv('995,000_rows.csv', chunksize=chunksize, low_memory=False):
     i+=1
     print('Processing chunk: {}'.format(i - 1))
-    chunk = chunk.dropna(subset=['id'])
-    print('Chunk Column: {} and {}'.format('id', chunk['id'].apply(type).unique()))
-    chunk['id'] = chunk['id'].astype(float)
-    # if i >= 35:
-        # print(chunk['id'])
-    # print('Chunk after Column: {} and {}'.format('id', chunk['id'].apply(type).unique()))
-    # Merge the chunk with df2 on the 'id' column
+    chunk = chunk.dropna(subset=['id', 'type', 'domain', 'content', 'title'])
+    chunk = chunk.drop(columns=['keywords', 'source', 'tags', 'meta_description', 'summary'])
+    chunk['authors'] = chunk['authors'].fillna('<none>')
+    chunk['meta_keywords'] = chunk['meta_keywords'].fillna('<none>')
+    chunk['id'] = chunk['id'].astype('string')
     merged_chunk = pd.merge(df2, chunk.drop(columns='content'), on='id', how='inner')
     # Append the merged chunk to the list
     dfs.append(merged_chunk)
