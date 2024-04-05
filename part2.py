85d5cf6c0c3580bcbb803bd5f8f85671d99b70df
Yaser Hashimi <yh@cerebriu.com>
2024-04-05 14:58:23 +0200

    added graphs

--- part2.py
+++ part2.py
@@ -1,45 +1,60 @@
-import dataprocessing
 import pandas as pd
 from collections import Counter
+import ast
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 # Part 2
 print('Part 2')
-filename = "995,000_rows.csv"
-chunksize = 1000
-total_chunks = 995000 // chunksize
+filename = "data/merged_dataset.csv"
+filename2 = "data/995,000_rows.csv"
+chunksize = 10000
+total_chunks = 245286 // chunksize
 print(total_chunks)
-# Initialize the word counts
+
+# Initialize the Counters
 word_counts_pre_processed = Counter()
 word_counts_post_processed = Counter()
+type_counts = Counter()
 i = 0
 # Get raw data and for processing
-for raw_data_chunk in pd.read_csv(filename, chunksize=chunksize, low_memory=False):
-      # raw_data_chunk = dataprocessing.get_data(chunk)
+for data_chunk in pd.read_csv(filename, chunksize=chunksize, low_memory=False):
       i+=1
       print(i)
-      # Drop empty rows and fill in empty authors and meta_keywords with <none>
-      raw_data_chunk = raw_data_chunk.dropna(subset=['id', 'type', 'domain', 'content', 'title'])
-      raw_data_chunk = raw_data_chunk.drop(columns=['keywords', 'source', 'tags', 'meta_description', 'summary'])
-      raw_data_chunk['authors'] = raw_data_chunk['authors'].fillna('<none>')
-      raw_data_chunk['meta_keywords'] = raw_data_chunk['meta_keywords'].fillna('<none>')
-      # Convert the data types to string, small memory optimization
-      raw_data_chunk = raw_data_chunk.astype('string')
+      # print (data_chunk['content'].head(5))
+      data_chunk['content'] = data_chunk['content'].apply(ast.literal_eval)
+      data_chunk['content'].apply(word_counts_post_processed.update)
+      
+most_common_words = word_counts_post_processed.most_common(1000)
+word_freq = dict(most_common_words)
 
-      # Work on an copy that gets cleaned
-      cleaned_data_chunk = raw_data_chunk.copy()
-      cleaned_data_chunk['content'] = cleaned_data_chunk['content'].apply(dataprocessing.text_preprocessing)
+# Generate the word cloud
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_freq)
 
-      # Tokenize the content
-      raw_data_chunk['content'] = raw_data_chunk['content'].apply(dataprocessing.tokenize)
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')  # Hide the axes
+plt.title('1000 Most Frequent Words')
+plt.savefig('most_frequent_words_wordcloud.png')
 
-      # Get word counts and reduction rate
-      word_counts_pre_processed = dataprocessing.count_words_extended(raw_data_chunk, 'content', word_counts_pre_processed)
-      word_counts_post_processed = dataprocessing.count_words_extended(cleaned_data_chunk, 'content', word_counts_post_processed)
+# most_common_words = word_counts_post_processed.most_common(1000)
+# words = [word for word, count in most_common_words]
+# counts = [count for word, count in most_common_words]
 
+# plt.figure(figsize=(10, 15))
+# plt.barh(words[:50], counts[:50])  # Display only the top 50 words for clarity
+# plt.gca().invert_yaxis()
+# plt.xscale('log')  # Set the x-axis to logarithmic scale
+# plt.xlabel('Count (log scale)')
+# plt.ylabel('Word')
+# plt.title('Top 50 out of 1000 Most Frequent Words')
+# plt.savefig('most_frequent_words_log_scale.png')
+
+# print(type_counts)
 # Compute the reduction rate and size of the vocabulary
-unique_word_count_pre = len(word_counts_pre_processed)
-unique_word_count_post = len(word_counts_post_processed)
-reduction_rate = dataprocessing.compute_reduction_rate(unique_word_count_pre, unique_word_count_post)
-print('Unique words before text processing: {},'
-      ' after text processing: {}'
-       ' and the reduction rate is: {:.2f}%'.format(unique_word_count_pre, unique_word_count_post, reduction_rate))
+# unique_word_count_pre = len(word_counts_pre_processed)
+# unique_word_count_post = len(word_counts_post_processed)
+# reduction_rate = dataprocessing.compute_reduction_rate(unique_word_count_pre, unique_word_count_post)
+# print('Unique words before text processing: {},'
+#       ' after text processing: {}'
+#        ' and the reduction rate is: {:.2f}%'.format(unique_word_count_pre, unique_word_count_post, reduction_rate))
